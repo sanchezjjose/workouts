52844f190f0c977abc58a3f7b50a02dc976e0167
Jose Sanchez <sanchezjjose@gmail.com>
2018-11-14 06:18:20 -0500

    Column heading improvements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,12 @@ class Home extends Component {
           <div className='content'>
             <h2 className='weekday'>Tuesday</h2>
             <div className='group'>
-              <h3 className='part'>Biceps</h3>
+              <div className='heading'>
+                <h3 className='part'>Biceps</h3>
+                <span className='weight'>Weight <span className='unit'>(lbs)</span></span>
+                <span className='reps'>Reps</span>
+                <span className='sets'>Sets</span>
+              </div>
               <div className='workout 1'>
                 <div className='type'>Curls (bands)</div>
                 <div className='weight'>-</div>
@@ -29,34 +34,39 @@ class Home extends Component {
               </div>
               <div className='workout 3'>
                 <div className='type'>Dumbbell Curls</div>
-                <div className='weight'>25 (lbs)</div>
+                <div className='weight'>25</div>
                 <div className='reps'>8</div>
                 <div className='sets'>3</div>
               </div>
             </div>
             <div className='group'>
-              <h3 className='part'>Triceps</h3>
+              <div className='heading'>
+                <h3 className='part'>Triceps</h3>
+                <span className='weight'>Weight <span className='unit'>(lbs)</span></span>
+                <span className='reps'>Reps</span>
+                <span className='sets'>Sets</span>
+              </div>
               <div className='workout 1'>
                 <div className='type'>Dips</div>
                 <div className='weight'>-</div>
-                <div className='reps'>15 (lbs)</div>
+                <div className='reps'>15</div>
                 <div className='sets'>3</div>
               </div>
               <div className='workout 2'>
                 <div className='type'>Tricep Pushdown (rope)</div>
-                <div className='weight'>110 (lbs)</div>
+                <div className='weight'>110</div>
                 <div className='reps'>8</div>
                 <div className='sets'>3</div>
               </div>
               <div className='workout 3'>
                 <div className='type'>Overhead Extension Machine</div>
-                <div className='weight'>60 (lbs)</div>
+                <div className='weight'>60</div>
                 <div className='reps'>8</div>
                 <div className='sets'>3</div>
               </div>
               <div className='workout 4'>
                 <div className='type'>EZ-Bar Standing Overhead</div>
-                <div className='weight'>70 (lbs)</div>
+                <div className='weight'>70</div>
                 <div className='reps'>8</div>
                 <div className='sets'>3</div>
               </div>
