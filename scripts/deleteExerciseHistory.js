653d4ac3746b3aa011e77b78b6de9a5b5e9ae732
Jose Sanchez <sanchezjjose@gmail.com>
2018-11-20 19:21:39 -0500

    Add delete exercise history functionality

---
+++ scripts/deleteExerciseHistory.js
@@ -0,0 +1,36 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+  // endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const userId = 'joses';
+const date = '11-20-2018';
+const muscle = 'Shoulders';
+const exercise = "Upright Cable Row";
+
+const params = {
+  TableName: 'Workouts',
+  Key: {
+    'id': userId
+  },
+  UpdateExpression: `REMOVE history.#d.#m.#e`,
+  ExpressionAttributeNames: {
+    "#d": date,
+    "#m": muscle,
+    "#e": exercise
+  },
+  ReturnValues:"ALL_NEW"
+};
+
+docClient.update(params, (err, data) => {
+  if (err) {
+    console.error('Unable to remove attribute from item. Error JSON:', JSON.stringify(err, null, 2));
+
+  } else {
+    console.log(data);
+  }
+});
