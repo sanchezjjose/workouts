4bba2e5be804b4584cd869d484f669da1ed2a7cb
josanche <jose.sanchez@disneystreaming.com>
2018-11-23 19:58:21 -0500

    Experimenting with different db structure

---
+++ scripts/addFavoriteExercise.js
@@ -0,0 +1,38 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+  // endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const userId = 'joses';
+const exercise = {
+  muscle: 'Chest',
+  name: 'Push-ups'
+};
+
+const params = {
+  TableName: 'Workouts',
+  Key: {
+    'id': userId
+  },
+  UpdateExpression: `SET exercises.#m = list_append(exercises.#m, :exercise)`,
+  ExpressionAttributeNames: {
+    "#m": "muscle"
+  },
+  ExpressionAttributeValues: {
+      ":exercise": [ exercise.name ]
+  },
+  ReturnValues:"ALL_NEW"
+};
+
+docClient.update(params, (err, data) => {
+  if (err) {
+    console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+
+  } else {
+    console.log(data);
+  }
+});
