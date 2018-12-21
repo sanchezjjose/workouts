9ea069bc0594b4d4ece201a043a0fe8da802d1e4
josanche <jose.sanchez@disneystreaming.com>
2018-12-21 21:15:13 +0100

    Update footer styles

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -11,7 +11,7 @@ class NavigationBar extends Component {
           <span class='title'>Workouts</span>
         </section>
         <section class='top-app-bar__section--align-end'>
-          <a href='#' class='material-icons mdc-top-app-bar__action-item' aria-label='Start Workout' alt='Start Workout'>start</a>
+          <a href='#' class='mdc-top-app-bar__action-item' aria-label='Start Workout' alt='Start Workout'>Start Workout</a>
           <a href='#' class='material-icons mdc-top-app-bar__action-item' aria-label='Edit' alt='Edit'>edit</a>
         </section>
       </header>
