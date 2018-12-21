7a7d4d6d68aa085132d2663328b662064bf37caf
josanche <jose.sanchez@disneystreaming.com>
2018-12-21 21:05:41 +0100

    New footer icosn

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Footer.css';
-import homeNavIcon from './home-nav-icon.svg';
-import progressNavIcon from './progress-nav-icon.svg';
 
 const Footer = ({ userId }) => {
 	return (
     <div className='Footer'>
       <Link className='link-home' to={`/${userId}`}>
         <div className='footer-button'>
-            <img src={homeNavIcon} className="tab-icon" alt="home" />
+            <button className="tab-icon mdc-icon-button material-icons">home</button>
             <span className='footer-text'> home </span>
         </div>
       </Link>
       <Link to={`/${userId}/favorites`}>
         <div className='footer-button'>
-            <img src={progressNavIcon} className="tab-icon" alt="progress" />
+            <button className="tab-icon mdc-icon-button material-icons">star_rate</button>
             <span className='footer-text'> favorites </span>
         </div>
       </Link>
