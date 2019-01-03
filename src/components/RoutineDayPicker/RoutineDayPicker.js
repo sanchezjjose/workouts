794e09a6c724cf1bc5ec9f9469e53c2661abd32b
Jose Sanchez <sanchezjjose@gmail.com>
2019-01-03 07:02:51 -0500

    Routine day picker styles

--- src/components/RoutineDayPicker/RoutineDayPicker.js
+++ src/components/RoutineDayPicker/RoutineDayPicker.js
@@ -10,7 +10,8 @@ class RoutineDayPicker extends Component {
 
   render() {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const dayOfweek = this.props.dayOfWeek;
+    const today = days[new Date().getDay()];
+    const selectedDay = this.props.dayOfWeek;
 
     return (
       <div className='RoutineDayPicker'>
@@ -18,7 +19,7 @@ class RoutineDayPicker extends Component {
           <span 
             key={day}
             onClick={() => this.handleClick(day)}
-            className={`routine-day ${dayOfweek === day ? 'active' : ''}`}>{day.charAt(0)}</span>
+            className={`routine-day ${today === day ? 'today' : ''} ${selectedDay === day ? 'active' : ''}`}>{day.charAt(0)}</span>
         )}
       </div>
     );
