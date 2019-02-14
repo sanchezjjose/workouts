1df0c040e495f86fb2afabfe26176c9f2fa93c99
Jose Sanchez <sanchezjjose@gmail.com>
2019-02-13 21:07:47 -0500

    Don't initialize particlejs on smaller devices

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -59,7 +59,7 @@ class Landing extends Component {
   }
 
   componentDidMount() {
-    if (typeof window.Particles === 'object') {
+    if (typeof window.Particles === 'object' && window.innerWidth >= 320) {
       window.Particles.init({ selector: '.Landing .background', connectParticles: true, speed: 0.3, maxParticles: 70 })
     }
   }
