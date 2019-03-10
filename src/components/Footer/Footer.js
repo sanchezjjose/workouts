241641d47edb6d3a9583ddfe6c2419ab28213efd
Jose Sanchez <sanchezjjose@gmail.com>
2019-03-09 23:15:08 -0500

    Change progress location in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -27,20 +27,20 @@ class Footer extends Component {
               <span className='footer-text'>home</span>
           </div>
         </Link>
-        <Link onClick={() => this.handleClick('progress')} className={`link-progress ${progressTabActive ? 'active' : '' }`} to={`/${userId}/progress`}>
-          <div className='footer-button'>
-              <button className="tab-icon mdc-icon-button material-icons">
-                {progressTabActive ? 'insert_chart' : 'insert_chart_outlined'}
-              </button>
-              <span className='footer-text'>progress</span>
-          </div>
-        </Link>
         <Link onClick={() => this.handleClick('favorites')} className={`link-favorites ${favTabActive ? 'active' : '' }`} to={`/${userId}/favorites`}>
           <div className='footer-button'>
               <button className="tab-icon mdc-icon-button material-icons">
                 {favTabActive ? 'star' : 'star_border'}
               </button>
               <span className='footer-text'>favorites</span>
+          </div>
+        </Link>
+        <Link onClick={() => this.handleClick('progress')} className={`link-progress ${progressTabActive ? 'active' : '' }`} to={`/${userId}/progress`}>
+          <div className='footer-button'>
+              <button className="tab-icon mdc-icon-button material-icons">
+                {progressTabActive ? 'insert_chart' : 'insert_chart_outlined'}
+              </button>
+              <span className='footer-text'>progress</span>
           </div>
         </Link>
       </div>
