b5bc95164742d1f9a1cdd450fd22e42dc756303a
josanche <jose.sanchez@disneystreaming.com>
2019-03-29 22:02:56 -0400

    Progress graph improvements

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -11,15 +11,21 @@ class Progress extends Component {
     this.progressByWorkoutChart = {};
   }
 
-  componentDidUpdate() {
+  setDefaults() {
     const colorMode = this.props.settings.getMode();
 
     if (colorMode === 'dark') {
       Chart.defaults.global.defaultFontColor = '#fff';
+      Chart.defaults.scale.gridLines.color = '#757575';
+
     } else {
       Chart.defaults.global.defaultFontColor = '#000';
+      Chart.defaults.scale.gridLines.color = 'rgba(0, 0, 0, 0.1)';
     }
+  }
 
+  componentDidUpdate() {
+    this.setDefaults();
     this.progressByMonthChart.update();
     this.progressByWorkoutChart.update();
   }
@@ -28,11 +34,8 @@ class Progress extends Component {
     const ctx1 = document.getElementById("progress-workouts-by-month");
     const ctx2 = document.getElementById("progress-workouts-by-weight");
     const progress = new ProgressCharts(this.props.history);
-    const colorMode = this.props.settings.getMode();
 
-    if (colorMode === 'dark') {
-      Chart.defaults.global.defaultFontColor = '#fff';
-    }
+    this.setDefaults();
 
     this.progressByMonthChart = new Chart(ctx1, {
       type: 'bar',
@@ -41,24 +44,16 @@ class Progress extends Component {
         datasets: [{
           label: '# of Workouts',
           data: progress.workoutsByMonth(),
-          backgroundColor: '#EF5350',
-          borderColor: '#EF5350',
+          backgroundColor: 'rgb(255, 99, 132, 0.5)',
+          borderColor: 'rgb(255, 99, 132)',
           borderWidth: 1
         }]
       },
       options: {
         scales: {
-          xAxes: [{
-            gridLines: {
-              color: '#757575'
-            }
-          }],
           yAxes: [{
-            gridLines: {
-              color: '#757575'
-            },
             ticks: {
-              beginAtZero:true
+              beginAtZero: true
             }
           }]
         }
@@ -79,16 +74,12 @@ class Progress extends Component {
             ticks: {
               source: 'data',
               autoSkip: true,
-              // beginAtZero: true
+              beginAtZero: true
             }
           }],
           yAxes: [{
-            // scaleLabel: {
-            //   display: true,
-            //   labelString: 'Weight / Time'
-            // },
             ticks: {
-              // beginAtZero: true
+              beginAtZero: true
             }
           }]
         },
@@ -97,13 +88,9 @@ class Progress extends Component {
 					mode: 'index',
 					callbacks: {
 						label: function(tooltipItem, data) {
-							let label = data.datasets[tooltipItem.datasetIndex].label || '';
-							let unit = data.datasets[tooltipItem.datasetIndex].metricUnit || '';
-							if (label) {
-								label += ': ';
-							}
-							label += `${tooltipItem.yLabel} ${unit}`;
-							return label;
+							const label = data.datasets[tooltipItem.datasetIndex].label;
+              const unit = data.datasets[tooltipItem.datasetIndex].metricUnit;
+							return `${label}: ${tooltipItem.yLabel} ${unit}`;
 						}
 					}
 				}
@@ -116,13 +103,16 @@ class Progress extends Component {
       <div className='Progress'>
         <canvas id="progress-workouts-by-weight" width="400" height="400"></canvas>
         <canvas id="progress-workouts-by-month" width="400" height="400"></canvas>
-        {/* <div style={{position: 'relative', height: 100 + 'vh', width: 100 + 'vw'}}>
-          <canvas id="progress-workouts-by-weight"></canvas>
-        </div>
-        <div style={{position: 'relative', height: 100 + 'vh', width: 100 + 'vw'}}>
-          <canvas id="progress-workouts-by-month"></canvas>
-        </div> */}
       </div>
+
+      // <div className='Progress' style={{width: '100%', height: '50%'}}>
+      //   <div style={{position: 'relative', height: 100 + 'vh', width: 100 + 'vw'}}>
+      //     <canvas id="progress-workouts-by-weight"></canvas>
+      //   </div>
+      //   <div style={{position: 'relative', height: 100 + 'vh', width: 100 + 'vw'}}>
+      //     <canvas id="progress-workouts-by-month"></canvas>
+      //   </div>
+      // </div>
     );
   }
 }
