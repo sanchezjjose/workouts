459ac35f427b24e2f1bff32f44554a0fe7687e53
Jose Sanchez <sanchezjjose@gmail.com>
2019-05-16 07:18:52 -0400

    New component for favorites workout group name

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { UserContext } from '../UserContext';
-import FavoritesModal from '../FavoritesModal/FavoritesModal'
+import FavoritesModal from '../FavoritesModal/FavoritesModal';
+import FavoritesTitle from './FavoritesTitle';
 import { deleteWorkout } from '../../api/Workouts';
 
 import './Favorites.css';
@@ -11,7 +12,7 @@ class Favorites extends Component {
   state = {
     // TODO: remove from state, causes re-render
     message: '',
-    editFavoriteName: ''
+    editingText: false
   }
 
   displayMessage = (message) => {
@@ -34,25 +35,16 @@ class Favorites extends Component {
       });
   }
 
-  handleEdit(id) {
-    this.setState({ editFavoriteName: id });
-    this.context.updateMode(true, false, false);
-  }
-
-  componentDidUpdate() {
-    if (this.context.saveMode === true || this.context.cancelMode === true) {
-      this.setState({ editFavoriteName: '' });
-      this.context.updateMode(false, false, false);
-    }
+  handleEditingText = (editingText) => {
+    this.setState({ editingText: editingText });
   }
 
   render() {
-    const editFavoriteName = this.state.editFavoriteName;
-    const editMode = this.context.editMode && editFavoriteName.length === 0;
+    const editMode = this.context.editMode && !this.state.editingText;
     const workoutsVm = this.context.workouts.getViewModel();
 
     return (
-      <div className={`Favorites ${editFavoriteName.length > 0 ? 'editing' : ''}`}>
+      <div className={`Favorites ${this.state.editingText ? 'editing' : ''}`}>
         <div className='content-wrapper'>
           <div className='content'>
           {this.state.message.length > 0 &&
@@ -62,23 +54,13 @@ class Favorites extends Component {
           {workoutsVm.map (workoutVm =>
             workoutVm.workouts.map (workout =>
               <div key={workout.group} className='workouts'>
-                <div className='workout-group-heading'>
-                  <input type='text'
-                    className={`workout-title ${editFavoriteName === workout.group ? 'editing' : ''}`}
-                    placeholder='Add a group name'
-                    value={workout.group}
-                    onFocus={() => this.handleEdit(workout.group)}
-                    onClick={() => this.handleEdit(workout.group)}
-                    onChange={() => this.handleEdit(workout.group)}
-                    readOnly={false} />
-                  <span className='input-border'></span>
-                </div>
+                <FavoritesTitle workoutGroupName={workout.group} handleEditingText={this.handleEditingText} />
                 {workout.exercises.map(exercise =>
                   <div key={exercise.id} className={`workout-group ${editMode ? 'editing' : ''}`}>
                     {editMode &&
                       <button onClick={() => this.removeWorkout(exercise)} className="delete-button mdc-icon-button material-icons">clear</button>
                     }
-                    <div key={exercise.id} onClick={() => this.handleEdit(exercise.id)} className={`workout-label ${editFavoriteName === exercise.id ? 'editing' : ''}`}>{exercise.name}</div>
+                    <div key={exercise.id} className={`workout-label`}>{exercise.name}</div>
                   </div>
                 )}
               </div>
---
+++ src/components/Favorites/FavoritesTitle.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { UserContext } from '../UserContext';
+
+class FavoritesTitle extends Component {
+  static contextType = UserContext;
+
+  state = {
+    workoutGroupName: this.props.workoutGroupName,
+    editing: false
+  }
+
+  handleOnChange(e) {
+    this.setState({ workoutGroupName: e.target.value });
+  }
+
+  handleEdit() {
+    this.setState({ editing: true });
+    this.props.handleEditingText(true);
+    this.context.updateMode(true, false, false);
+  }
+
+  handleOnBlur() {
+    this.setState({ editing: false });
+  }
+
+  componentDidUpdate() {
+    if (this.context.saveMode === true || this.context.cancelMode === true) {
+      this.setState({ editing: false });
+      this.props.handleEditingText(false);
+      this.context.updateMode(false, false, false);
+    }
+  }
+
+  render() {
+    const workoutGroupName = this.state.workoutGroupName;
+
+    return (
+      <div className='workout-group-heading'>
+        <input type='text'
+          className={`workout-title ${this.state.editing ? 'editing' : ''}`}
+          placeholder='Add a group name'
+          value={workoutGroupName}
+          onClick={() => this.handleEdit()}
+          onFocus={() => this.handleEdit()}
+          onBlur={() => this.handleOnBlur()}
+          onChange={() => this.handleOnChange()}
+          readOnly={false} />
+        <span className='input-border'></span>
+      </div>
+    );
+  }
+}
+
+export default FavoritesTitle;
